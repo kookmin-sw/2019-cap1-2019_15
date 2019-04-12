2c97340d24e69f727dcf46f850d0a5db7e5f49ce
minin han <minin1995@naver.com>
2019-04-12 17:24:20 +0900

    frontend 수정

--- template/js/app.js
+++ template/js/app.js
@@ -1,14 +1,15 @@
-function cart(menu){
-	var x = document.getElementById('menu').value
+function cart(k){
+	var x = document.getElementById("k").value
 	if(confirm(x + "을(를) 장바구니에 담으시겠습니까?")){
 	 location.href="http://cdmanii.tistory.com";}
  else{
 	 alert('아니오를 누르셨습니다');
  }
-}
+}// x에다가 id로 전달하는거 수정하기
+
 function complete(){
 	if(confirm("주문을 완료하시겠습니까 ?")){
-	 location.href="http://cdmanii.tistory.com";}
+	 location.href="no-sidebar.html";}
  else{
 	 alert('아니오를 누르셨습니다');
  }
