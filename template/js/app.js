5d3ae43406308461795ff089c01e86e6446d7fb3
coco34521 <36881152+coco34521@users.noreply.github.com>
2019-05-07 04:16:08 +0900

    Add files via upload

--- template/js/app.js
+++ template/js/app.js
@@ -1,10 +1,11 @@
-function cart(k){
+function cart(value){
+	var k = value
 	if(confirm(k + "을(를) 장바구니에 담으시겠습니까?")){
-		location.href="http://google.co.kr";}
+		alert('추가되었습니다');}
  	else{
 	 	alert('아니오를 누르셨습니다');
  }
-}// x에다가 id로 전달하는거 수정하기
+}
 
 function complete(){
 	if(confirm("주문을 완료하시겠습니까 ?")){
