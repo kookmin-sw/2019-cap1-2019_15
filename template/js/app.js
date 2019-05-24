cd07d54eb101e108525b8d2ab300fdb732a672b7
coco34521 <36881152+coco34521@users.noreply.github.com>
2019-05-24 10:07:21 +0900

    Add files via upload

--- template/js/app.js
+++ template/js/app.js
@@ -1,16 +1,16 @@
-<<<<<<< HEAD
-function cart(){
-	var k = document.querySelector('p').getAttribute('data-product-id') ;
-	if(confirm(k + "을(를) 장바구니에 담으시겠습니까?")){
-		alert('추가되었습니다');}
- 	else{
-	 	alert('아니오를 누르셨습니다');
- }
-
-function complete(){
-	if(confirm("주문을 완료하시겠습니까 ?")){
-	 location.href="no-sidebar.html";}
- else{
-	 alert('아니오를 누르셨습니다');
- }
-}
+// function cart(value){
+// 	var k = value
+// 	if(confirm(k + "을(를) 장바구니에 담으시겠습니까?")){
+// 		alert('추가되었습니다');}
+//  	else{
+// 	 	alert('아니오를 누르셨습니다');
+//  }
+// }
+//
+// function complete(){
+// 	if(confirm("주문을 완료하시겠습니까 ?")){
+// 	 location.href="no-sidebar.html";}
+//  else{
+// 	 alert('아니오를 누르셨습니다');
+//  }
+// }
