dde1777eb226479dc4a2580aea68082bd2afdb33
YonHoonSense <yh0520@naver.com>
2019-05-30 10:27:45 +0900

    시선추적 방법 수정

--- src/eyetracking/main.py
+++ src/eyetracking/main.py
@@ -1,21 +1,57 @@
 from imutils import face_utils
 import numpy as np
+from scipy.spatial import distance as dist
 import argparse
 import imutils
-import pyautogui
 import dlib
 import cv2
-#from pymouse import PyMouse
-#m = PyMouse()
+from pymouse import PyMouse
+import time
+m = PyMouse()
 
+ANCHOR_POINT = (0, 0)
 
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
+
+(nStart, nEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+(lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
 
 
+def eye_distance(eye):
 
+	A = dist.euclidean(eye[1], eye[5])
+	B = dist.euclidean(eye[2], eye[4])
+
+
+	C = dist.euclidean(eye[0], eye[3])
+
+	eye_dist = (A + B) / (2.0 * C)
+
+	return eye_dist
+
+EYE_AR_THRESH = 0.28
+EYE_AR_CONSEC_FRAMES = 8
+COUNTER = 0
+TOTAL = 0
+
+
+def direction(r_eye_point, anchor_point, w, h1, h2, multiple=1):
+	nx, ny = r_eye_point
+	x, y = anchor_point
+
+	if ny > y + multiple * h1:
+		return 'down'
+	elif ny < y - multiple * h1:
+		return 'up'
+	elif ny > y + multiple * h2:
+		return 'down2'
+	elif ny < y - multiple * h2:
+		return 'up2'
+
+	return '-'
 
 past_values_x = []
 def min_intensity_x(img):
@@ -75,9 +111,8 @@ def extract_eye(image, left, bottom_left, bottom_right, right, upper_right, uppe
 
 	pupil_x = min_intensity_x(eye)
 	pupil_y = min_intensity_y(eye)
-	print("x: " ,pupil_x)
-	print("y: " ,pupil_y)
-	pyautogui.moveTo(800 - (pupil_x-19)*150, 450-(pupil_y-3)*80,0.2)
+	# print("x: " ,pupil_x)
+	# print("y: " ,pupil_y)
 
 	cv2.circle(eye,(pupil_x, pupil_y), 2, (0,255,0), -1)
 
@@ -88,52 +123,150 @@ def extract_eye(image, left, bottom_left, bottom_right, right, upper_right, uppe
 
 	return eye
 
+ANCHOR = 0
+
+
 while(True):
 	ret, image = cap.read()
-	image = imutils.resize(image, width=500)
+
 	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
+
 	rects = detector(gray, 1)
+	check = 0
 
 	for (i, rect) in enumerate(rects):
 		shape = predictor(gray, rect)
 		shape = face_utils.shape_to_np(shape)
 
+		r_eye = shape[nStart:nEnd]
+		r_eye_point = (r_eye[3, 0], r_eye[3, 1])
+
+		while ANCHOR < 10:
+			ANCHOR += 1
+			ANCHOR_POINT = r_eye_point
+
+
+
+
+
+
+		x, y = ANCHOR_POINT
+		nx, ny = r_eye_point
+		w= 60
+		h1= 10
+		h2 =10
+		h= 10
+		multiple = 1
+		cv2.line(image, ANCHOR_POINT, r_eye_point, (0,0,255), 2)
+
+		dir = direction(r_eye_point, ANCHOR_POINT, w, h1, h2)
+		#cv2.putText(image, dir.upper(), (10, 90), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255,0,0), 2) 3840
+		drag = 18
+		xx = list(m.position())
+		xxx = xx[0]
+		yyy = xx[1]
+		# if dir == 'up':
+		# 	m.move(xxx, 395)
+		# # elif dir == 'up2':
+		# # 	m.move(xxx,400)
+		# elif dir == 'down':
+		# 	m.move(xxx,1580)
+		# # elif dir == 'down2' :
+		# # 	m.move(xxx,3300)
+		# else :
+		# 	m.move(xxx,1065)
+
+
+
+		leftEye = shape[lStart:lEnd]
+		rightEye = shape[nStart:nEnd]
+		leftEAR = eye_distance(leftEye)
+		rightEAR = eye_distance(rightEye)
+
+
+		ear = (leftEAR + rightEAR) / 2.0
+
+
+		if ear < EYE_AR_THRESH:
+			COUNTER += 1
+
+
+		else:
+
+			if COUNTER >= EYE_AR_CONSEC_FRAMES:
+				TOTAL += 1
+				m.click(xxx,yyy,1)
+
+				cv2.putText(right_eye, "Click!", (10, 30),
+					cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+
+
+			COUNTER = 0
 
 
 
 		count = 1
+		right_eye = imutils.resize(extract_eye(image, shape[36], shape[41], shape[46], shape[45], shape[44], shape[37]), width=200, height=100)
+		#left_eye = imutils.resize(extract_eye(image, shape[42], shape[47], shape[46], shape[45], shape[44], shape[43]), width=200, height=100)
 
-		right_eye = imutils.resize(extract_eye(image, shape[36], shape[41], shape[40], shape[39], shape[38], shape[37]), width=100, height=50)
-		left_eye = imutils.resize(extract_eye(image, shape[42], shape[47], shape[46], shape[45], shape[44], shape[43]), width=100, height=50)
+		rows, cols, _ = right_eye.shape
+		right_eye = right_eye[0:1000,10:60] # cut right_eye
 
-		right_center_x = int(((shape[37])[0] + (shape[38])[0] + (shape[41])[0] + (shape[40])[0]) / 4)
-		right_center_y = int(((shape[37])[1] + (shape[38])[1] + (shape[41])[1] + (shape[40])[1]) / 4)
+		gray_right_eye = cv2.cvtColor(right_eye, cv2.COLOR_BGR2GRAY)
+		gray_right_eye = cv2.GaussianBlur(gray_right_eye, (9, 9), 0)
+		_, threshold = cv2.threshold(gray_right_eye, 45, 255, cv2.THRESH_BINARY_INV)
+		contours, _ = cv2.findContours(threshold, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+		contours = sorted(contours, key=lambda x: cv2.contourArea(x), reverse=True)
 
-		left_center_x = int(((shape[43])[0] + (shape[44])[0] + (shape[46])[0] + (shape[47])[0]) / 4)
-		left_center_y = int(((shape[43])[1] + (shape[44])[1] + (shape[46])[1] + (shape[47])[1]) / 4)
+		cv2.line(right_eye,(250,300),(250,400),(255,0,0),1)
+		cv2.line(right_eye,(200,350),(300,350),(255,0,0),1)
+		# print(contours)
 
-		#print("Right = ", right_eye , right_eye)
-		#print("Left = ", left_center_x, left_center_y)
+		# x = cv2.boundingRect(contours[0])
+		# y = cv2.boundingRect(contours[1])
+		# w = cv2.boundingRect(contours[2])
+		# h = cv2.boundingRect(contours[3])
 
-		#m.move(800 - (right_center_x - right_eye),450 - (right_center_y - right_eye))
+		for cnt in  contours :
+			(x, y, w, h) = cv2.boundingRect(cnt)
+			cv2.drawContours(right_eye, [cnt], -1, (0, 0, 255), 3)
+			cv2.rectangle(right_eye, (x, y), (x + w, y + h), (255, 0, 0), 2)
+			cv2.line(right_eye, (x + int(w/2), 0), (x + int(w/2), rows), (0, 255, 0), 2)
+			cv2.line(right_eye, (0, y + int(h/2)), (cols, y + int(h/2)), (0, 255, 0), 2)
 
-		for (x, y) in shape:
+			#print((int(y)+int(h/2)))
 
-			if count > 36 and count < 43:
-				cv2.circle(image, (x, y), 1, (255, 0, 0), -1)
-			if count > 42 and count < 49:
-				cv2.circle(image, (x, y), 1, (255, 0, 0), -1)
+			#move mouse 1920 4080
+			print((int(x)+int(w/2)))
+			xx = list(m.position())
+			xxx = xx[0]
+			yyy = xx[1]
+			print(xx)
+			if((int(x)+int(w/2)) > 30 ):
+				m.move(400,yyy)
+			elif((int(x)+int(w/2)) < 20):
+				m.move(1400,yyy)
+			else :
+				m.move(960,yyy)
 
-			count += 1
+			break
+			# for cnt in contour0s:
+				# (x, y, w, h) = cv2.boundingRect(cnt)
+				# print(cnt,"dddddddd")
+				# print(x,y,w,h)
 
 
 
-		image[0:len(right_eye),0:len(right_eye[0])] = right_eye
-		image[0:len(left_eye),0:len(left_eye[0])] = left_eye
 
-	cv2.imshow("Frame", image)
+		check = 1
+		# cv2.imshow("Threshold", threshold)
+		# #cv2.imshow("gray right_eye", gray_right_eye)
+		# cv2.imshow("right_eye", right_eye)
+		cv2.imshow("image",image)
 
+	if check == 0:
+		ANCHOR =0
 
 	key = cv2.waitKey(1)
 	if key == 27:
